022ac1951a1832e57eeda3d913222f1489df1f52
Roger Camargo <huogerac@gmail.com>
2023-04-14 20:20:43 -0300

    fix linter

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/service/log_svc.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/service/log_svc.py
@@ -1,10 +1,9 @@
 from ..models import ActivityLog
-import json
 
 
 def log_login(user):
     logs = ActivityLog(
-        type='login',
+        type="login",
         logged_user=user,
     )
     logs.save()
@@ -12,7 +11,7 @@ def log_login(user):
 
 def log_logout(user):
     logs = ActivityLog(
-        type='logout',
+        type="logout",
         logged_user=user,
     )
     logs.save()
