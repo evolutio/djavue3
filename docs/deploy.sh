19b06dae969d3e8880fffdbdf20ff97b25b79029
Roger Camargo <huogerac@gmail.com>
2023-10-05 21:51:43 -0300

    vai

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -4,10 +4,11 @@
 set -e
 
 # build
-npm run docs:build
+cd docs/
+npm run build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
+cd .vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
