e738e90deca63f15644cbcd114eb6695f7223728
Luciano Martins <zicadopv@gmail.com>
2023-10-11 21:31:28 -0300

    issue#47 - Adiciona rota /status e test_status.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/base/views.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/base/views.py
@@ -1,3 +1,5 @@
+from django.db import connection
+from django.http import JsonResponse
 {% if cookiecutter.django_api == "openapi" %}
 from django.shortcuts import render
 {% elif cookiecutter.django_api == "django_ninja" %}
@@ -13,6 +15,14 @@ class Error(Schema):
 @router.get("/dapau", response={500: Error}){% endif %}
 def dapau(request):
     raise Exception("break on purpose")
+
+
+{% if cookiecutter.django_api == "django_ninja" %}@router.get("/status"){% endif %}
+def status(request):
+    cursor = connection.cursor()
+    cursor.execute("""SELECT 1+1""")
+    row = cursor.fetchone()
+    return JsonResponse({"status": "ok", "db": "ok" if row == (2,) else "error"})
 
 
 {% if cookiecutter.django_api == "openapi" %}
