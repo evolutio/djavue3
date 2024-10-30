07f5e16d0c5994f81704fd8bc4eafe7d9576f26b
Roger Camargo <huogerac@gmail.com>
2024-10-29 22:14:40 -0300

    Issue #117 Inicio de um prompt melhor sem perguntar várias vezes o nome do model

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,7 @@
+"""
+
+{{ cookiecutter.update({"model_lower": cookiecutter.model.lower() }) }}
+{{ cookiecutter.update({"model_singular": cookiecutter.model[:-1] }) }}
+{{ cookiecutter.update({"model_singular_lower": cookiecutter.model_singular.lower() }) }}
+
+"""
